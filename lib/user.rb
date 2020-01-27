928541f9cd9905d778c57f3c922df8ae19d14310
PabloSKZ <pablo.eskenazi@gmail.com>
2020-01-27 14:14:12 +0100

    exemple POO

---
+++ lib/user.rb
@@ -0,0 +1,36 @@
+require "pry"
+class User
+  attr_accessor :email, :encrypted_password
+  @@user_count = 0
+
+  def initialize(email_to_save)
+    @email = email_to_save
+    @@user_count = @@user_count + 1
+  end
+
+  def change_password(new_password)
+    @encrypted_password = encrypt(new_password)
+  end
+
+  def show_itself
+    puts self
+  end
+
+  def self.count
+    return @@user_count
+  end
+
+  private
+
+  def encrypt(string_to_encrypt)
+    return "##ENCRYPTED##"
+  end
+
+end
+
+
+pablo = User.new("pablo@blow.com")
+aziz = User.new("aziz@blow.com")
+
+binding.pry
+puts "end of file"
